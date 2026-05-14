46794f0de6cb7f7118056e27134bc69d1eba1fad
aKaPi80 <alvarocalvo8@gmail.com>
2026-05-14 22:32:11 +0200

    Update exam-core.mjs

--- exam-core.mjs
+++ exam-core.mjs
@@ -1,6 +1,6 @@
 ﻿﻿export const SUPABASE_URL = 'https://zipfwmmwcawfbqofhwmc.supabase.co';
 export const SUPABASE_KEY = 'sb_publishable_j1dhehxot0jJ98uUNblN4A_c3ujTqn6';
-export const EXAM_SHEET_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbxXJu7d3Rzxl8kWLqKoZrt7vt0eLbOiE2tP5JOEJnYHoBMJq7g8CPEyZ_rbPI8xNsns/exec';
+export const EXAM_SHEET_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbzZmohwuAVf24287ng6s_W05wJflhZbXOGJpLysLH26wYC8ZkSSrrYpqWiJuEjcIB6I/exec';
 
 export const grades = [
   ['5kyu', '5 KYU (Amarillo)'],
